0d0fe3b6ad53cb3e17a4d38854684213acfd0e96
MarcoCondinanzo <marcocondinanzo@gmail.com>
2022-05-10 23:33:09 -0300

    algo

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ require('dotenv').config();
 
 const publicPath = path.resolve(__dirname, './public');
 app.use(express.static(publicPath));
-const port= process.env.PORT | 3000; 
+const port= process.env.PORT || 3000; 
 
 app.listen( port , () => {
     console.log('Servidor funcionando');

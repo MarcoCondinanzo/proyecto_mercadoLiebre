6d127284e200e5937ca3341825fedb70803ee937
MarcoCondinanzo <marcocondinanzo@gmail.com>
2022-05-10 22:52:30 -0300

    agregado Procfile y puerto por variable de entorno

--- app.js
+++ app.js
@@ -4,9 +4,9 @@ const path = require('path');
 
 const publicPath = path.resolve(__dirname, './public');
 app.use(express.static(publicPath));
+const port= process.env.PORT | 3000; 
 
-
-app.listen(3000, () => {
+app.listen( port , () => {
     console.log('Servidor funcionando');
 });
 
